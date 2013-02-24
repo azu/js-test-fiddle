6fa90c46c8b88d588bda0706565f41a68b8b24af
azu <azuciao@gmail.com>
2013-02-24 18:01:49 +0900

    fix jasmine-start.js

--- dist/externals/jasmine-start.js
+++ dist/externals/jasmine-start.js
@@ -1,13 +1,14 @@
 (function () {
-    var jasmineEnv = jasmine.getEnv();
-    jasmineEnv.updateInterval = 1000;
-    var htmlReporter = new jasmine.HtmlReporter();
-    jasmineEnv.addReporter(htmlReporter);
-    jasmineEnv.specFilter = function (spec) {
-        return htmlReporter.specFilter(spec);
-    };
     var currentWindowOnload = window.onload;
     window.onload = function () {
+        var jasmineEnv = jasmine.getEnv();
+        jasmineEnv.updateInterval = 1000;
+        var htmlReporter = new jasmine.HtmlReporter();
+        jasmineEnv.addReporter(htmlReporter);
+        jasmineEnv.specFilter = function (spec) {
+            return htmlReporter.specFilter(spec);
+        };
+
         if (currentWindowOnload) {
             currentWindowOnload();
         }

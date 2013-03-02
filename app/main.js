509fac571a8bdd34270397c1ef55a787f57c2e70
azu <azuciao@gmail.com>
2013-03-03 00:57:59 +0900

    exact js/css

--- app/main.js
+++ app/main.js
@@ -8,13 +8,13 @@ var app = {};
         var form = document.createElement("form");
         form.setAttribute("method", method);
         form.setAttribute("action", path);
+        form.setAttribute("target", "_blank");
         for (var key in params) {
             if (params.hasOwnProperty(key)) {
                 var hiddenField = document.createElement("input");
                 hiddenField.setAttribute("type", "hidden");
                 hiddenField.setAttribute("name", key);
                 hiddenField.setAttribute("value", params[key]);
-
                 form.appendChild(hiddenField);
             }
         }
@@ -27,25 +27,26 @@ var app = {};
 })();
 
 jQuery(function ($) {
-    var elemIDs = ["qunit", "jasmine", "mocha", "busterjs"];
+    var elemIDs = ["qunit", "jasmine", "mocha-bdd", "busterjs"];
     var units = {
         "qunit": {
             "html": $("#template-qunit-html").html(),
             "js": $("#template-qunit-js").html(),
+            "wrap": "l",//=> onload
             "resources": ["http://code.jquery.com/qunit/qunit-git.js", "http://code.jquery.com/qunit/qunit-git.css"]
         },
         "jasmine": {
             "html": $("#template-jasmine-html").val(),
             "js": $("#template-jasmine-js").html(),
-            "js_wrap": "l",//=> onload - Post API does not implement…
+            "wrap": "l",//=> onload
             "resources": [
                 "http://pivotal.github.com/jasmine/lib/jasmine.css"
             ]
         },
-        "mocha": {
+        "mocha-bdd": {
             "html": '<div id="mocha"></div>',
-            "js": $("#template-mocha-js").html(),
-            "js_wrap": "l",//=> onload - Post API does not implement…
+            "js": $("#template-mocha-bdd-js").html(),
+            "wrap": "l",//=> onload
             "resources": [
                 "https://raw.github.com/LearnBoost/expect.js/master/expect.js",
                 "https://raw.github.com/visionmedia/mocha/master/mocha.js",
@@ -54,7 +55,7 @@ jQuery(function ($) {
         },
         "busterjs": {
             "js": $("#template-busterjs-js").html(),
-            "js_wrap": "l",//=> onload - Post API does not implement…
+            "wrap": "l",//=> onload
             "resources": [
                 "http://cdn.busterjs.org/releases/latest/buster-test.js",
                 "http://cdn.busterjs.org/releases/latest/buster-test.css",
